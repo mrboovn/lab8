e0949e884b5888a5adde9ca956b9e028d1d2301b
Minh Nguyen <m9nguyen@ucsd.edu>
2021-02-25 13:26:07 -0800

    push

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = '8HUGliM1yAKjJGR4MbPbfv32u8Mc9X2Y';
+
+	var map = L.mapquest.map('map', {
+		center: [32.878481, - 117.235814],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	// Add a marker to the map
+	L.marker([32.878481, -117.235814]).addTo(map);
 }
